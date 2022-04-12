ace95cbb3fb98679c36351b8459993ecb4d07a1a
ocosmin <ocosmin@gmail.com>
2022-04-12 15:08:52 +0300

    Add Night Mode Button

--- app/src/main/java/eu/fastapps/test2/FeaturesActivity.java
+++ app/src/main/java/eu/fastapps/test2/FeaturesActivity.java
@@ -1,17 +1,35 @@
 package eu.fastapps.test2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class FeaturesActivity extends AppCompatActivity {
+
+    Button btnDarkMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_features);
+
+        btnDarkMode = findViewById(R.id.btn_dark_mode);
+        btnDarkMode.setOnClickListener(
+                (b)-> onToggleDarkMode()
+        );
+
     }
 
+    private void onToggleDarkMode() {
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+        else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+    }
 
 
 }
