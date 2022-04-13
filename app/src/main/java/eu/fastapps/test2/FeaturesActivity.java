3133ac7eb98d4c76e6266445fa6e77f359326049
ocosmin <ocosmin@gmail.com>
2022-04-13 12:16:49 +0300

    Add Youtube button

--- app/src/main/java/eu/fastapps/test2/FeaturesActivity.java
+++ app/src/main/java/eu/fastapps/test2/FeaturesActivity.java
@@ -3,12 +3,16 @@ package eu.fastapps.test2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class FeaturesActivity extends AppCompatActivity {
 
     Button btnDarkMode;
+    Button btnYT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,19 @@ public class FeaturesActivity extends AppCompatActivity {
                 (b)-> onToggleDarkMode()
         );
 
+        btnYT = findViewById(R.id.btn_youtube);
+        btnYT.setOnClickListener(
+                (b)-> onYoutubeLink()
+        );
+
     }
+
+    private void onYoutubeLink(){
+        Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.youtube.com/watch?v=" + "saEpkcVi1d4"));
+        startActivity(webIntent);
+    }
+
 
     private void onToggleDarkMode() {
         if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
